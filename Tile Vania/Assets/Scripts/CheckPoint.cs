3b6c9984fb100b0958e666e5f04038f310a07e48
Hammas Tahir <hammastahir11@gmail.com>
2022-08-17 16:21:09 +0500

    .....

--- Tile Vania/Assets/Scripts/CheckPoint.cs
+++ Tile Vania/Assets/Scripts/CheckPoint.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+
+  
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Newplayer")
         {
           //  Player.Respawnpoint = transform.position;
         }
+        
     }
+
+  
 }
